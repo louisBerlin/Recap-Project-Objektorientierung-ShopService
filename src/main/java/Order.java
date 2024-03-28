5ce7092f2e282c9ebb8c72efabcc1b4ac551ba22
louisBerlin <louis.froch@gmail.com>
2024-03-28 09:19:17 +0100

    Add new status in Order.java

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public record Order(
         String id,
+
+        String PROCESSING,
+        String IN_DELIVERY,
+        String COMPLETED,
         List<Product> products
 ) {
 }
